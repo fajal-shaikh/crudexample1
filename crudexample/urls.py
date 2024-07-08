5c0d4534e77f8c0d1f2a98099e34d5f9f295d3a2
Priyanka <priyanka.syadav@cloverinfotech.com>
2024-07-08 10:39:37 +0530

    first commit

---
+++ crudexample/urls.py
@@ -0,0 +1,15 @@
+# crudexample/urls.py
+
+from django.contrib import admin
+from django.urls import path
+from employee import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('emp', views.emp, name='emp'),
+    path('show', views.show, name='show'),
+    path('edit/<int:id>', views.edit, name='edit'),
+    path('update/<int:id>', views.update, name='update'),
+    path('delete/<int:id>', views.destroy, name='delete'),
+    path('', views.show, name='home'),  # Add a name for the root URL
+]
